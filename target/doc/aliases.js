46915b155ce802481a03b5733c9db9cbabdfaf32
Jacob Brown <kardeiz@gmail.com>
2019-10-07 13:59:00 -0500

    docs new

---
+++ target/doc/aliases.js
@@ -0,0 +1,7 @@
+var ALIASES = {};
+ALIASES["fast_map"] = {};
+ALIASES["fast_map_derive"] = {};
+ALIASES["proc_macro2"] = {};
+ALIASES["quote"] = {};
+ALIASES["syn"] = {};
+ALIASES["unicode_xid"] = {};
---
+++ target/doc/implementors/fast_map/easy/trait.MapLike.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["fast_map"] = [];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
